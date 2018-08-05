5bf68234e7456bc9c214cbaef156694d288922d0
vvishthanaphop <42110390+vvishthanaphop@users.noreply.github.com>
2018-08-05 16:52:05 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, request
+import json
+import requests
+
+# ตรง YOURSECRETKEY ต้องนำมาใส่เองครับจะกล่าวถึงในขั้นตอนต่อๆ ไป
+global LINE_API_KEY
+LINE_API_KEY = 'Bearer 3e14350cfbd0125d9a7e22e4610db72a'
+
+app = Flask(__name__)
+ 
+@app.route('/')
+def index():
+    return 'This is chatbot server.'
+@app.route('/bot', methods=['POST'])
+
+def bot():
+    # ข้อความที่ต้องการส่งกลับ
+    replyStack = list()
+   
+    # ข้อความที่ได้รับมา
+    msg_in_json = request.get_json()
+    msg_in_string = json.dumps(msg_in_json)
+    
+    # Token สำหรับตอบกลับ (จำเป็นต้องใช้ในการตอบกลับ)
+    replyToken = msg_in_json["events"][0]['replyToken']
+
+    # ทดลอง Echo ข้อความกลับไปในรูปแบบที่ส่งไป-มา (แบบ json)
+    replyStack.append(msg_in_string)
+    reply(replyToken, replyStack[:5])
+    
+    return 'OK',200
+ 
+def reply(replyToken, textList):
+    # Method สำหรับตอบกลับข้อความประเภท text กลับครับ เขียนแบบนี้เลยก็ได้ครับ
+    LINE_API = 'https://api.line.me/v2/bot/message/reply'
+    headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': LINE_API_KEY
+    }
+    msgs = []
+    for text in textList:
+        msgs.append({
+            "type":"text",
+            "text":text
+        })
+    data = json.dumps({
+        "replyToken":replyToken,
+        "messages":msgs
+    })
+    requests.post(LINE_API, headers=headers, data=data)
+    return
+
+if __name__ == '__main__':
+    app.run()
